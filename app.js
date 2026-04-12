1f14417bedcfb177d3201296a6da1adbafb8afea
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-04-12 17:59:24 +0530

    feat: Added index, show, add, create routes

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 8080;
-const Listing = require("../Zariya/models/listing.js")
+const Listing = require("../Zariya/models/listing.js");
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 
 main()
@@ -17,19 +22,44 @@ app.get("/", (req, res) => {
   res.send("Hi, I'm root");
 });
 
+// index route
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find({});
+  res.render("listing/index.ejs", {allListings});
+})
+
+// add route
+app.get("/listings/new", (req, res) => {
+  res.render("listing/new.ejs");
+})
+
+// create route
+app.post("/listings", async (req, res) => {
+  let newListing = await new Listing(req.body.listing);
+  newListing.save();
+  res.redirect("/listings");
+})
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+  let {id} = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listing/show.ejs", {listing})
+})
+
 // sample testing
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "Mehmaan",
-    description: "By the Beach",
-    price: 120000,
-    location: "Baga Beach",
-    country: "India"
-  });
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("Successful!")
-});
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "Mehmaan",
+//     description: "By the Beach",
+//     price: 120000,
+//     location: "Baga Beach",
+//     country: "India"
+//   });
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("Successful!")
+// });
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
